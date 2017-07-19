792cdcfec9a4198b734eb624291ec4b1d00fa403
Tuanhao <tuanhao256@gmail.com>
2017-07-18 23:03:33 -0400

    added redirect with passing data

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  let attempt
+  console.log(req.query)
+  req.query.attempt == null ? attempt = 0 : attempt = req.query.attempt
+  res.render('index', { title: 'Express', attempt });
 });
 
 module.exports = router;
--- routes/logging.js
+++ routes/logging.js
@@ -1,10 +1,21 @@
 var express = require('express');
 var router = express.Router();
+const url = require('url');
 
 /* GET logging page where all the logging info will be shown */
-router.get('/', function(req, res, next) {
-  res.render('logging');
-  console.log(req.body)
+router.post('/', function(req, res, next) {
+  var body = req.body;
+  if (body.firstname == "Hao" && body.lastname == "Chau" && body.password == "123") {
+    res.render('logging');
+  } else {
+    let newAttempt = Number(req.body.attempt) + 1
+    res.redirect(url.format({
+      pathname:"/",
+      query: {
+        attempt: newAttempt
+      }
+    }))
+  }
 });
 
 module.exports = router;
